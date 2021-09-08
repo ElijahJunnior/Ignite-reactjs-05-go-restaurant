60a1c2d1c71e96b948aa52ae3379ffe6eaf82978
Elias Junior <elijah.junnior@outlook.com>
2021-09-07 22:32:11 -0300

    the remaining files were changed to use TypeScript

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import api from '../../services/api';
+import { api } from '../../services/api';
 
 import { Header } from '../../components/Header'
 import { Food } from '../../components/Food';
